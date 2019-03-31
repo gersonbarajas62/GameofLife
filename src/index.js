e4d34521a7e8fb1f437adee20674efe80a4eb79f
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-03-31 10:44:17 -0600

    setup of the proyect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Grid extends React.Component {
+    render() {
+        return(
+            <div>
+                Grid
+            </div>
+        );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    }
+}
+class Main extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            generation : 0,
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Game Of Life</h1>
+                <grid
+                />
+                <h2>Generations: {this.state.generation} </h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Main /> , document.getElementById('root'));
+
+
+ 
