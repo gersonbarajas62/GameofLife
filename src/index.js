e7a7d482a2fe0a1113403a287707d285866fd59a
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-01 12:53:03 -0600

    box class and grid class created

--- src/index.js
+++ src/index.js
@@ -2,21 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Box extends React.Component{
+  selectBox = () => {
+    this.props.seloectBox(this.props.row.props.col);
+  }
+  render(){
+    return(
+      <div
+      className={this.props.boxClass}
+      id={this.props.id}
+      onClick={this.selectBox}
+      />
+    );
+  }
+}
+
 class Grid extends React.Component {
     render() {
-        return(
-            <div>
-                Grid
-            </div>
-        );
+      const width = this.props.cols -14
+      const rowsArr = []
 
-    }
+         var boxClass = "";
+         for(let i = 0; i < this.props.rows; i++){
+              for(let j = 0; j < this.props.cols; j++){
+                let boxId = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+                rowsArr.push(
+                  <box
+                  boxclass={boxClass}
+                  key={boxId}
+                  row={i}
+                  col={j}
+                  selectBox={this.props.selectBox}
+                  />
+                )
+              }
+         }
+
+
+        return (
+         <div className="grid" style={{width: width}}>
+        {{rowsArr}}
+         </div>
+        );
+   }
 }
+
 class Main extends React.Component{
     constructor() {
         super();
+        this.speed = 100;
+        this.rows = 30;
+        this.col = 50;
+
         this.state = {
             generation : 0,
+            gr4idFull: Array(this.rows).fill(() =>(this.col).fill(false)
+          )
         }
     }
     render() {
@@ -24,6 +67,10 @@ class Main extends React.Component{
             <div>
                 <h1>The Game Of Life</h1>
                 <grid
+                  gridFull={this.state.gridFull}
+                  rows={this.rows}
+                  col={this.col}
+                  selectBox={this.selectBox}
                 />
                 <h2>Generations: {this.state.generation} </h2>
             </div>
@@ -32,6 +79,3 @@ class Main extends React.Component{
 }
 
 ReactDOM.render(<Main /> , document.getElementById('root'));
-
-
- 
